85bc8e979aafa156d03cb2738a6adadee68e4c84
Noorbala <A.Noorbala@gmail.com>
2021-01-25 10:37:55 +0330

    Building the Web Front-End Step 2 of 11

--- modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
@@ -1,30 +1,28 @@
 package com.liferay.docs.guestbook.portlet.portlet;
 
-import com.liferay.docs.guestbook.portlet.constants.GuestbookWebPortletKeys;
-
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import org.osgi.service.component.annotations.Component;
 
 import javax.portlet.Portlet;
-
-import org.osgi.service.component.annotations.Component;
 
 /**
  * @author noor
  */
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=GuestbookWeb",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + GuestbookWebPortletKeys.GUESTBOOKWEB,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+		immediate = true,
+		property = {
+				"com.liferay.portlet.display-category=category.social",
+				"com.liferay.portlet.instanceable=false",
+				"com.liferay.portlet.scopeable=true",
+				"javax.portlet.display-name=Guestbook",
+				"javax.portlet.expiration-cache=0",
+				"javax.portlet.init-param.template-path=/",
+				"javax.portlet.init-param.view-template=/guestbook/view.jsp",
+				"javax.portlet.resource-bundle=content.Language",
+				"javax.portlet.security-role-ref=power-user,user",
+				"javax.portlet.supports.mime-type=text/html"
+		},
+		service = Portlet.class
 )
 public class GuestbookWebPortlet extends MVCPortlet {
 }
